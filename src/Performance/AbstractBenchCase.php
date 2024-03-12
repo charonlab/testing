8aa573fb6b84bca792f5ee34952bb7bf1b5c3517
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-03-12 17:24:44 +0100

    feat: add abstract test cases

---
+++ src/Performance/AbstractBenchCase.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the charonlab/testing.
+ *
+ * Copyright (C) 2023-2024 Charon Lab Development Team
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE.md file for details.
+ */
+
+namespace Charon\Testing\Performance;
+
+abstract class AbstractBenchCase
+{
+
+}
---
+++ src/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the charonlab/testing.
+ *
+ * Copyright (C) 2023-2024 Charon Lab Development Team
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE.md file for details.
+ */
+
+namespace Charon\Testing;
+
+use \PHPUnit\Framework\TestCase as PHPPUnitTestCase;
+
+abstract class TestCase extends PHPPUnitTestCase
+{
+
+}
